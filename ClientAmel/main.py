ef15c380f0a067480fe345a49c2fd0101deb63a9
Amel-Dzogovic <tOKYO-2020>
2021-06-09 07:47:22 +0200

    auslesen der pc daten und publishen auf broker

--- ClientAmel/main.py
+++ ClientAmel/main.py
@@ -1,5 +1,8 @@
+import time
 import paho.mqtt.client as paho
 import platform,socket,re,uuid,json,psutil,logging
+
+
 broker="broker.mqttdashboard.com"
 def on_publish(client,userdata,result):
     print("data published \n")
@@ -10,21 +13,24 @@ def getSystemInfo():
     info['platform']=platform.system()
     info['platform-release']=platform.release()
     info['platform-version']=platform.version()
-    ##info['architecture']=platform.machine()
-    ##info['hostname']=socket.gethostname()
-    ##info['ip-address']=socket.gethostbyname(socket.gethostname())
-    ##info['mac-address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
+    info['architecture']=platform.machine()
+    info['hostname']=socket.gethostname()
+    info['ip-address']=socket.gethostbyname(socket.gethostname())
+    info['mac-address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
     info['processor']=platform.processor()
+    info['CPU auslastung']=psutil.cpu_percent()
     info['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
+    info['used ram']=psutil.virtual_memory().percent
+    info['laufzeit']=time.process_time()
     return info
-
-
 
 information = getSystemInfo()
 
-client1= paho.Client("control1")
-client1.on_publish = on_publish
-client1.connect(broker)
-ret= client1.publish("test/bulb1",information.__str__())
+client1= paho.Client("ServerStatus")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while True:
+    client1.on_publish = on_publish
+    client1.connect(broker)
+    ret= client1.publish("test/HTLLeonding/MQTT/NCA",information.__str__())
+    time.sleep(10)
+
