8e710e9c66757f0aeb9eca17f56490ae53ae08e1
Amel-Dzogovic <tOKYO-2020>
2021-06-09 08:33:46 +0200

    hi

--- ClientAmel/main.py
+++ ClientAmel/main.py
@@ -1,36 +1,38 @@
 import time
 import paho.mqtt.client as paho
-import platform,socket,re,uuid,json,psutil,logging
+import platform, socket, re, uuid, psutil
+
+broker = "broker.mqttdashboard.com"
 
 
-broker="broker.mqttdashboard.com"
 def on_publish(client,userdata,result):
     print("data published \n")
     pass
 
+
 def getSystemInfo():
-    info={}
-    info['platform']=platform.system()
-    info['platform-release']=platform.release()
-    info['platform-version']=platform.version()
-    info['architecture']=platform.machine()
-    info['hostname']=socket.gethostname()
-    info['ip-address']=socket.gethostbyname(socket.gethostname())
-    info['mac-address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
-    info['processor']=platform.processor()
-    info['CPU auslastung']=psutil.cpu_percent()
-    info['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
-    info['used ram']=psutil.virtual_memory().percent
-    info['laufzeit']=time.process_time()
+    info = {}
+    info['platform'] = platform.system()
+    info['platform-release'] = platform.release()
+    info['platform-version'] = platform.version()
+    info['architecture'] = platform.machine()
+    info['hostname'] = socket.gethostname()
+    ##info['ip-address'] = socket.gethostbyname(socket.gethostname())
+    ##info['mac-address'] = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+    info['processor'] = platform.processor()
+    info['CPU auslastung'] = psutil.cpu_percent()
+    info['ram'] = str(round(psutil.virtual_memory().total / (1024.0 ** 3))) + " GB"
+    info['ram asulastung'] = psutil.virtual_memory().percent
     return info
 
-information = getSystemInfo()
 
-client1= paho.Client("ServerStatus")
+
+
+client = paho.Client("ServerStatus")
 
 while True:
-    client1.on_publish = on_publish
-    client1.connect(broker)
-    ret= client1.publish("test/HTLLeonding/MQTT/NCA",information.__str__())
+    information = getSystemInfo()
+    client.on_publish = on_publish
+    client.connect(broker)
+    ret = client.publish("test/HTLLeonding/MQTT/NCA", information.__str__())
     time.sleep(10)
-
